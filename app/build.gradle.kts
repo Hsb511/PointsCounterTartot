aef6e79bf1c53ffd48f54f132c04b24cca76f938
HSA86E4N <hugo-externe.selle@enedis.fr>
2022-05-04 22:59:02 +0200

    [UPD] Settings plugins & dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -34,10 +36,33 @@ android {
 }
 
 dependencies {
+    // Core
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("com.google.android.material:material:1.5.0")
+
+    // Compose libraries
+    implementation("androidx.compose.compiler:compiler:${Versions.COMPOSE}")
+    implementation("androidx.compose.runtime:runtime-livedata:${Versions.COMPOSE}")
+    implementation("androidx.compose.material:material:${Versions.COMPOSE}")
+    implementation("androidx.compose.ui:ui:${Versions.COMPOSE}")
+    implementation("androidx.compose.ui:ui-viewbinding:${Versions.COMPOSE}")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.1.1")
+    implementation("androidx.compose.ui:ui-tooling:${Versions.COMPOSE}")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0-beta01")
+    implementation("androidx.navigation:navigation-compose:2.4.2")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:${Versions.HILT}")
+    kapt("com.google.dagger:hilt-android-compiler:${Versions.HILT}")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
+
+kapt {
+    correctErrorTypes = true
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,4 +1,5 @@
 object Versions {
+    const val COMPOSE = "1.2.0-alpha08"
     const val KOTLIN = "1.6.21"
     const val HILT = "2.41"
 }
