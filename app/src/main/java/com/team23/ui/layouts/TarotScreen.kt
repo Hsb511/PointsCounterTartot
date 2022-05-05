29a4f3b990e06ef8dc28356b6126ab7e33d6bcb6
HSA86E4N <hugo-externe.selle@enedis.fr>
2022-05-05 18:17:18 +0200

    [ADD] Adding a Floating Action Button to add a new game

--- app/src/main/java/com/team23/ui/layouts/TarotScreen.kt
+++ app/src/main/java/com/team23/ui/layouts/TarotScreen.kt
@@ -1,6 +1,5 @@
 package com.team23.ui.layouts
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,8 +7,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,25 +34,42 @@ fun TarotScreen(
     totalScores: List<Int>,
     scores: List<List<Int>>
 ) {
-    Column {
-        Card(
-            shape = RoundedCornerShape(8.dp),
-            modifier = Modifier
-                .padding(8.dp)
-                .border(1.dp, MaterialTheme.colors.primaryVariant, shape = RoundedCornerShape(8.dp))
-        ) {
-            LazyColumn(
-                modifier = Modifier.fillMaxWidth()
+    Scaffold(
+        modifier = Modifier.padding(8.dp),
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* TODO NAVIGATE TO THE SCREEN OF NEW GAME */ },
+                elevation = FloatingActionButtonDefaults.elevation(8.dp)
             ) {
-                item {
-                    GridHeader(playersName = playersName, totalScores = totalScores)
-                }
-                items(scores) {
-                    GridContent(scores = it)
+                Icon(Icons.Filled.Add, "Add")
+            }
+        }) { padding ->
+        Column {
+            Card(
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier
+                    .padding(padding)
+                    .border(
+                        1.dp,
+                        MaterialTheme.colors.primaryVariant,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+            ) {
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    item {
+                        GridHeader(playersName = playersName, totalScores = totalScores)
+                    }
+                    items(scores) {
+                        GridContent(scores = it)
+                    }
                 }
             }
         }
     }
+
+
 }
 
 @Preview(showSystemUi = true)
