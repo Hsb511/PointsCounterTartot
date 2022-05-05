e73cde34389b7c832f008507b11eb69ecb478c7b
HSA86E4N <hugo-externe.selle@enedis.fr>
2022-05-05 15:24:56 +0200

    [ADD] Creating the grid view and the data in the vm

--- app/src/main/java/com/team23/ui/layouts/TarotScreen.kt
+++ app/src/main/java/com/team23/ui/layouts/TarotScreen.kt
@@ -1,7 +1,11 @@
 package com.team23.ui.layouts
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,21 +18,51 @@ import com.team23.ui.viewmodels.TarotViewModel
 
 @Composable
 fun TarotScreen(tarotViewModel: TarotViewModel = viewModel()) {
-    TarotScreen()
+    TarotScreen(
+        playersName = tarotViewModel.playersName.value,
+        totalScores = tarotViewModel.totalScores.value,
+        scores = tarotViewModel.scores.value
+    )
 }
 
 @Composable
-fun TarotScreen() {
-    Text(text = "Tarot",
-        style = MaterialTheme.typography.h5,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
+fun TarotScreen(
+    playersName: List<String>,
+    totalScores: List<Int>,
+    scores: List<List<Int>>
+) {
+    Column {
+        Text(
+            text = "Tarot",
+            style = MaterialTheme.typography.h5,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        )
+        LazyColumn (modifier = Modifier
             .padding(8.dp)
-            .fillMaxWidth())
+            .fillMaxSize()
+        ) {
+            item {
+                Text(text = playersName.toString())
+            }
+            item {
+                Text(text = totalScores.toString())
+            }
+            items(scores) {
+                Text(text = it.toString())
+            }
+        }
+    }
 }
 
 @Preview(showSystemUi = true)
 @Composable
 fun TarotScreenPreview() {
-    TarotScreen()
+    TarotScreen(
+        playersName = listOf("Laure", "Romane", "Guillaume", "Justine", "Hugo"),
+        totalScores = listOf(1000, 1000, 1000, 1000, 1000),
+        scores = listOf(listOf(-23, -23, -23, 23, 23), listOf(23, 23, 23, -23, -23))
+    )
 }
--- app/src/main/java/com/team23/ui/viewmodels/TarotViewModel.kt
+++ app/src/main/java/com/team23/ui/viewmodels/TarotViewModel.kt
@@ -1,9 +1,20 @@
 package com.team23.ui.viewmodels
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class TarotViewModel @Inject constructor() : ViewModel() {
+    val playersName: MutableState<List<String>> = mutableStateOf(emptyList())
+    val totalScores: MutableState<List<Int>> = mutableStateOf(emptyList())
+    val scores: MutableState<List<List<Int>>> = mutableStateOf(emptyList())
+
+    init {
+        playersName.value = listOf("Laure", "Romane", "Guillaume", "Justine", "Hugo")
+        totalScores.value = listOf(1000, 1000, 1000, 1000, 1000)
+        scores.value = listOf(listOf(-23, -23, -23, 23, 23), listOf(23, 23, 23, -23, -23))
+    }
 }
