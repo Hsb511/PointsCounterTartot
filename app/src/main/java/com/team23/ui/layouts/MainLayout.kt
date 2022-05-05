36fb6ccaa7ca3ec6cc6e5f11a5f97323f0f1c6e8
HSA86E4N <hugo-externe.selle@enedis.fr>
2022-05-05 14:40:43 +0200

    [ADD] Creating the tarotViewModel

--- app/src/main/java/com/team23/ui/layouts/MainLayout.kt
+++ app/src/main/java/com/team23/ui/layouts/MainLayout.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.team23.ui.viewmodels.TarotViewModel
 
 @Composable
 fun MainLayout() {
@@ -14,7 +16,7 @@ fun MainLayout() {
 
     Box(modifier = Modifier.fillMaxSize()) {
         NavHost(navController, startDestination = "tarot") {
-            composable(route = "tarot") { TarotScreen() }
+            composable(route = "tarot") { TarotScreen(hiltViewModel()) }
         }
     }
 }
--- app/src/main/java/com/team23/ui/layouts/TarotScreen.kt
+++ app/src/main/java/com/team23/ui/layouts/TarotScreen.kt
@@ -9,6 +9,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.team23.ui.viewmodels.TarotViewModel
+
+@Composable
+fun TarotScreen(tarotViewModel: TarotViewModel = viewModel()) {
+    TarotScreen()
+}
 
 @Composable
 fun TarotScreen() {
