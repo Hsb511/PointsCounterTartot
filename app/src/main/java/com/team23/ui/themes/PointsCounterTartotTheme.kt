f2f1ab0debe48be8bc635c293c31b7d3b99da747
HSA86E4N <hugo-externe.selle@enedis.fr>
2022-05-05 15:41:58 +0200

    [ADD] Setting a personalize UI Theme

---
+++ app/src/main/java/com/team23/ui/themes/PointsCounterTartotTheme.kt
@@ -0,0 +1,18 @@
+package com.team23.ui.themes
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors()
+private val LightColorPalette = lightColors()
+
+@Composable
+fun PointsCounterTartotTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) { DarkColorPalette } else { LightColorPalette }
+
+    MaterialTheme(colors = colors, typography = Typography(), shapes = Shapes(), content = content)
+}
