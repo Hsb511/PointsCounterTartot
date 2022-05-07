1d146bf66213954bff7a6184c7eddb6825149fcf
HSA86E4N <hugo-externe.selle@enedis.fr>
2022-05-07 18:01:25 +0200

    [ADD] Adding a button to create a new game

--- app/src/main/java/com/team23/ui/components/HomeCard.kt
+++ app/src/main/java/com/team23/ui/components/HomeCard.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,58 +35,69 @@ inline fun HomeCard(
             .fillMaxSize()
             .padding(8.dp)
     ) {
-        Column(modifier = Modifier.padding(4.dp)) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.h6,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(0.dp, 4.dp, 0.dp, 8.dp)
-            )
+        Scaffold(
+            modifier = Modifier.padding(4.dp),
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { /* TODO */ },
+                    elevation = FloatingActionButtonDefaults.elevation(8.dp)
+                ) {
+                    Icon(Icons.Filled.Add, "Add")
+                }
+            }) { padding ->
+            Column(modifier = Modifier.padding(padding).fillMaxWidth()) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(0.dp, 4.dp, 0.dp, 8.dp)
+                )
 
-            LazyColumn {
-                items(games) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(4.dp)
-                            .background(
-                                color = MaterialTheme.colors.secondary,
-                                shape = RoundedCornerShape(16.dp)
+                LazyColumn {
+                    items(games) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(4.dp)
+                                .background(
+                                    color = MaterialTheme.colors.primary,
+                                    shape = RoundedCornerShape(16.dp)
+                                )
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Person,
+                                contentDescription = "players amount",
+                                modifier = Modifier.padding(8.dp, 8.dp, 0.dp, 8.dp)
                             )
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.Person,
-                            contentDescription = "players amount",
-                            modifier = Modifier.padding(8.dp, 8.dp, 0.dp, 8.dp)
-                        )
-                        Text(
-                            text = it.players.size.toString(),
-                            modifier = Modifier.padding(0.dp, 8.dp, 8.dp, 8.dp)
-                        )
-                        Icon(
-                            imageVector = Icons.Filled.DateRange,
-                            contentDescription = "creation date",
-                            modifier = Modifier.padding(0.dp, 8.dp)
-                        )
-                        Text(
-                            text = it.getSmallDate(),
-                            modifier = Modifier.padding(4.dp, 8.dp, 8.dp, 8.dp)
-                        )
-                        Icon(
-                            imageVector = Icons.Filled.Refresh,
-                            contentDescription = "game rounds",
-                            modifier = Modifier.padding(0.dp, 8.dp)
-                        )
-                        Text(
-                            text = "${it.scores.size} ${stringResource(R.string.home_round)}",
-                            modifier = Modifier.padding(4.dp, 8.dp)
-                        )
+                            Text(
+                                text = it.players.size.toString(),
+                                modifier = Modifier.padding(0.dp, 8.dp, 8.dp, 8.dp)
+                            )
+                            Icon(
+                                imageVector = Icons.Filled.DateRange,
+                                contentDescription = "creation date",
+                                modifier = Modifier.padding(0.dp, 8.dp)
+                            )
+                            Text(
+                                text = it.getSmallDate(),
+                                modifier = Modifier.padding(4.dp, 8.dp, 8.dp, 8.dp)
+                            )
+                            Icon(
+                                imageVector = Icons.Filled.Refresh,
+                                contentDescription = "game rounds",
+                                modifier = Modifier.padding(0.dp, 8.dp)
+                            )
+                            Text(
+                                text = "${it.scores.size} ${stringResource(R.string.home_round)}",
+                                modifier = Modifier.padding(4.dp, 8.dp)
+                            )
+                        }
                     }
                 }
-            }
 
-            content()
+                content()
+            }
         }
     }
 }
